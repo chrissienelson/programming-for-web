3e7f22c289bc320dcf6c2eef0b25edbc97c65455
Chrissie Nelson <cnelson315@mcad.edu>
2021-01-25 18:54:17 -0600

    array demo

---
+++ week2/arrays.js
@@ -0,0 +1,27 @@
+// Arrays
+const myMovies = [];
+console.log(myMovies);
+
+const mySongs = ['happy birthday song', 'twinkle twinkle', 'itsy bitsy spider'];
+console.log(mySongs);
+
+const myNumbers = (1, 2, 3, 4, 5);
+console.log(myNumbers);
+
+//viewing values by index
+console.log(mySongs[2]);
+
+// get length
+console.log(mySongs.length - 1);
+
+//push
+myNumbers.push(13);
+console.log(myNumbers);
+
+//pop
+// mySongs.pop();
+// mySongs.pop();
+// console.log(mySongs[mySongs.length - 1]);
+
+
+//shift
