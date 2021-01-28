34bc089cd8a0e1b8764b532c3630bfc656cba8ed
Chrissie Nelson <cnelson315@mcad.edu>
2021-01-27 18:53:44 -0600

    arrays exercise

--- week2/arrays.js
+++ week2/arrays.js
@@ -1,27 +1,15 @@
+const newmovie = window.prompt('What is your favorite movie?');
+
 // Arrays
-const myMovies = [];
+const myMovies = [
+    "The Greatest Showman",
+    "Bridesmaids",
+    "Someone Great",
+    "I Love You Man",
+    "Forgetting Sarah Marshall]"
+];
 console.log(myMovies);
 
-const mySongs = ['happy birthday song', 'twinkle twinkle', 'itsy bitsy spider'];
-console.log(mySongs);
+myMovies.push(newmovie);
+console.log(myMovies.toString());
 
-const myNumbers = (1, 2, 3, 4, 5);
-console.log(myNumbers);
-
-//viewing values by index
-console.log(mySongs[2]);
-
-// get length
-console.log(mySongs.length - 1);
-
-//push
-myNumbers.push(13);
-console.log(myNumbers);
-
-//pop
-// mySongs.pop();
-// mySongs.pop();
-// console.log(mySongs[mySongs.length - 1]);
-
-
-//shift
---
+++ week2/exercise.js
@@ -0,0 +1,36 @@
+const newmovie = window.prompt('What is your favorite movie?');
+
+// Arrays
+const myMovies = [
+    "The Greatest Showman",
+    "Bridesmaids",
+    "Someone Great",
+    "I Love You Man",
+    "Forgetting Sarah Marshall]"
+
+
+console.log(myMovies);
+
+console.log(myMovies.toString());
+
+const mySongs = ['happy birthday song', 'twinkle twinkle', 'itsy bitsy spider'];
+console.log(mySongs);
+
+const myNumbers = (1, 2, 3, 4, 5);
+console.log(myNumbers);
+
+//viewing values by index
+console.log(mySongs[2]);
+
+// get length
+console.log(mySongs.length - 1);
+
+//push
+myNumbers.push(13);
+console.log(myNumbers);
+
+//pop
+// mySongs.pop();
+// mySongs.pop();
+// console.log(mySongs[mySongs.length - 1]);
+//shift
