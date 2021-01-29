fbc1614fbd1f83825015ef034599c8e3ba43c926
Chrissie Nelson <cnelson315@mcad.edu>
2021-01-28 20:44:31 -0600

    trial and error

--- week2/games.js
+++ week2/games.js
@@ -1,9 +1,36 @@
 const games = window.prompt("I have three games in my collection. Pick a number between 1 and 3, and I'll tell you about that game!");
 
 const myGames = [
-    { game: "Scrabble", numberOfPlayers: 4, format: "a board", description: "is a word game in which two to four players score points by placing tiles, each bearing a single letter, onto a game board divided into a 15×15 grid of squares." },
-    { game: "Sequence", numberOfPlayers: 2, format: "a board, cards", description: "is an abstract strategy board-and-card game. The object of the game is to form rows of five poker chips on the board by placing the chips on the board spaces corresponding to cards played from the player's hand." },
-    { game: "Catch Phrase", numberOfPlayers: 6, format: "electronic", description: "is the frenzied game of fast talking and fast passing. Get the word or phrase, and rattle off as many clues as you can until your team guesses it." },
+    { 
+        game: "Scrabble", 
+        numberOfPlayers: 2, 
+        format: "a board", 
+        description: "is a board game made up of connecting letters and spelling words." 
+    },
+
+    { 
+        game: "Jenga", 
+        numberOfPlayers: 2, 
+        format: "blocks", 
+        description: "is a a building block balancing game." 
+    },
+
+    { 
+        game: "Catch Phrase", 
+        numberOfPlayers: 6, 
+        format: "electronic", 
+        description: "is a very intense game when played in a large group." 
+    },
 ];
 
-console.log("You selected " + myGames[games - 1].game + ", which is a " + myGames[games - 1].numberOfPlayers + " player game" + " and is played using " + myGames[games - 1].format + ". It " + myGames[games - 1].description);
+window.alert(
+    "You selected " +
+      myGames[games - 1].game +
+      ", which is a " +
+      myGames[games - 1].players +
+      " player game" +
+      " and is played using a " +
+      myGames[games - 1].type +
+      ". It is " +
+      myGames[games - 1].shortDescription
+  );
