05ac2af79f66292b9b65e3576988bc1de36584a5
Chrissie Nelson <cnelson315@mcad.edu>
2021-02-04 20:11:32 -0600

    playing around with pattern options

--- week3/pattern.js
+++ week3/pattern.js
@@ -1,36 +1,36 @@
 function setup() {
     createCanvas(600, 600);
 }
-
-function createTile(originX, originY) {
+function createTile(originX, originY, primaryColor, 
+secondaryColor, circleOutlineColor, dotColor) {
     translate(originX, originY);
-    fill('blue');
+    fill(secondaryColor);
     rect(0, 0 ,200, 200);
-    stroke('white');
+    stroke(secondaryColor);
     strokeWeight(5);
     line(0, 0, 200, 200);
     line(200, 0, 0, 200);
     noStroke();
-    fill('yellow');
+    fill(dotColor);
+    rect(50, 50, 100);
     circle(100, 50, 50);
     circle(50, 100, 50);
     circle(150, 100, 50);
     circle(100, 150, 50);
-    noFill();
-    stroke('purple');
+    stroke(circleOutlineColor);
     strokeWeight(5);
-    circle(100, 100, 100);
+
 
 }
 
 function draw(){
-    createTile(0,0);
-    createTile(0,200);
-    createTile(0,200);
-    createTile(200,-400);
-    createTile(0,200);
-    createTile(0,200);
-    createTile(200,-400);
-    createTile(0,200);
-    createTile(0,200);
+    createTile(0,0,'green', 'pink', 'cyan', 'magenta');
+    createTile(0,200,'green', 'yellow', 'cyan', 'green');
+    createTile(0,200,'green', 'pink', 'cyan', 'magenta');
+    createTile(200,-400,'green', 'yellow', 'cyan', 'green');
+    createTile(0,200, 'green', 'pink', 'cyan', 'magenta');
+    createTile(0,200, 'green', 'yellow', 'cyan', 'green');
+    createTile(200,-400,'green', 'pink', 'cyan', 'magenta');
+    createTile(0,200, 'green', 'yellow', 'cyan', 'green');
+    createTile(0,200, 'green', 'pink', 'cyan', 'magenta');
 }
