90b49a68e2849cfca01b514dcfa0c710b3a1ac12
Chrissie Nelson <cnelson315@mcad.edu>
2021-02-08 18:47:07 -0600

    radial exercise

--- week3/radial.js
+++ week3/radial.js
@@ -1,3 +1,28 @@
+let rotateBy = 5;
 function setup() {
     createCanvas(600, 600);
+    background (0);
+    angleMode(DEGREES);
+}
+function makeArm(rotateBy) {
+    let alt = Math.round(rotateBy / 360);
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+    stroke('pink');
+    rect(100, 100 + alt, 100 / alt);
+}
+
+function draw() {
+    translate(300, 300)
+    rotate(rotateBy)
+    makeArm(rotateBy);
+    rotateBy += 3;
+}
+function mousePressed() {
+    noLoop();
+}
+
+function doubleClicked() {
+    loop();
 }
