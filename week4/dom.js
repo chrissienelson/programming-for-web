9d9c1d674f2156164da54056cc64245a03dd3f62
Chrissie Nelson <cnelson315@mcad.edu>
2021-02-11 19:02:58 -0600

    create exercise 4.2 files

--- week4/dom.js
+++ week4/dom.js
@@ -3,7 +3,7 @@ const myEmptyDiv = document.querySelector("#myEmptyDiv");
 // put an h1 inside of it
 //create a new element
 const heading = document.createElement('h1');
-heading.innerHTML = 'Hello!';
+heading.innerHTML = 'What do you get when you cross a turtle and a llama? A turtleneck sweater';
 heading.style.cursor = 'pointer';
 heading.addEventListener('click', handleHeadingClick);
 myEmptyDiv.appendChild(heading);
