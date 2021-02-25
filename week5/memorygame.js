34c776c41100b485f30967c8a7690ef6335ad35a
Chrissie Nelson <cnelson315@mcad.edu>
2021-02-24 20:01:24 -0600

    memory game assignment

--- week5/memorygame.js
+++ week5/memorygame.js
@@ -6,17 +6,35 @@ let cards = [];
 const gameState = {
 
 };
+let cardfaceArray = [];
 let cardback;
 function preload() {
     cardback = loadImage('images/meow-cardback.png')
+    cardfaceArray = [
+        loadImage('images/kitten1.jpg'),
+        loadImage('images/kitten2.jpg'),
+        loadImage('images/kitten3.jpg'),
+        loadImage('images/kitten4.jpg'),
+        loadImage('images/kitten5.jpg')
+    ]
 
 }
 function setup() {
     createCanvas(900, 500);
     background('cyan');
+    let selectedFaces = [];
+    for (let z = 0; z < 5; z++) {
+        const randomIdx = floor(random(cardfaceArray.length));
+        const face = cardfaceArray[randomIdx];
+        selectedFaces.push(face);
+        selectedFaces.push(face);
+        // remove the used cardface so it doesn't get randomly selected again
+        cardfaceArray.splice(randomIdx, 1);
+    }
     for (let j = 0; j < 2; j++) {
         for (let i = 0; i < 5; i++) {
-            cards.push(new Card(startingX, startingY));
+            const faceImage = selectedFaces.pop();
+            cards.push(new Card(startingX, startingY, faceImage));
             startingX += 150;
         }
         startingY += 150;
@@ -33,12 +51,13 @@ function mousePressed() {
 }
 
 class Card {
-    constructor (x, y) {
+    constructor (x, y, cardFaceImg) {
         this.x = x;
         this.y = y;
         this.width = 80;
         this.height= 100;
         this.face = DOWN;
+        this.cardFaceImg = cardFaceImg;
         this.show();
     }
 
@@ -50,6 +69,7 @@ class Card {
         } else {
             fill('#aaa');
             rect(this.x, this.y, this.width, this.height, 10);
+            image(this.cardFaceImg, this.x + 5, this.y + 4); 
         }
 
     }
@@ -71,4 +91,19 @@ class Card {
         }
         this.show();
     } 
+}
+
+function shuffleArray (array) {
+    let counter = array.length;
+    while (counter > 0) {
+        // Pick random index
+        const idx = Math.floor(Math.random() * counter);
+        // decrease counter by 1 (decrement)
+        counter--;
+        // swap the last element with it
+        const temp = array[counter];
+        array[counter] = array[idx];
+        array[idx] = temp;
+    }
+    return array;
 }
