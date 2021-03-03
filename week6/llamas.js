24e9ddbedc015a108e26c87bbb2111ca70b8f295
Chrissie Nelson <cnelson315@mcad.edu>
2021-03-03 17:44:58 -0600

    Llama Vue Assignment

---
+++ week6/llamas.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#llamaFact",
+    data: {
+        llamaMember: "Llamas are members of the camelid family.",
+        llamaSmart: "Llamas are smart and easy to train.",
+        llamaVeg: "Llamas are vegetarians and have very efficient digestive systems.",
+        llamaPoop: "Llama poop has almost no odor. ",
+        llamaColor: "Llamas come in a range of solid and spotted colors including black, gray, beige, brown, red, and white.",
+        llamaBite: "Llamas don't bite. They spit when they're agitated, but that's mostly at each other.",
+        llamaLife: "Llamas live to be about 20 years old.",
+        llamaSocial: "Llamas are social animals and prefer to live with other llamas or herd animals.",
+        llamaWeight: "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight.",
+        llamaHeight: "Llamas can grow as much as 6 feet tall."
+    }
+});
