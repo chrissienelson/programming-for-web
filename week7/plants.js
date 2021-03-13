ec87a3142f16d54a85b0d9ae7c98026ea93cf9e2
Chrissie Nelson <cnelson315@mcad.edu>
2021-03-13 09:36:07 -0600

    update plants.js

--- week7/plants.js
+++ week7/plants.js
@@ -6,27 +6,27 @@ let vm = new Vue({
             name: "Snake Plant",
             botanicalName: "Sansevieria trifasciata",
             light: "low to bright indirect light",
-            petFriendly: “No”,
+            petFriendly: "No",
         }, {
             name: "Monstera",
             botanicalName: "Monstera deliciosa",
             light: "bright indirect to full sun",
-            petFriendly: “No”,
+            petFriendly: "No",
         }, {
             name: “Fiddle leaf Fig”,
             botanicalName: "Ficus lyrata",
             light: "bright indirect light",
-            petFriendly: “No”,
+            petFriendly: "No",
         }, {
             name: 'Money Tree',
             botanicalName: "Pachira aquatica",
             light: "low to bright indirect light",
-            petFriendly: “Yes”,
+            petFriendly: "Yes",
         }, {
             name: 'Bamboo Palm',
             botanicalName: "Chamaedorea seifrizii",
             light: "low to bright indirect light",
-            petFriendly: “Yes”,
+            petFriendly: "Yes",
         },
       ]
     }
