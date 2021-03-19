1d31e194f3689ebc0d5f7ba9e7b73c466a9bbd34
Chrissie Nelson <cnelson315@mcad.edu>
2021-03-18 22:09:56 -0500

    exercise 8.2

---
+++ week8/calculator.js
@@ -0,0 +1,33 @@
+const vm = new Vue({
+    el: '#calcApp',
+    data: {
+        firstNum: null,
+        secondNum: null,
+        operation: 'add',
+        total: null
+    },
+    methods: {
+        calculate: (num1, num2) => {
+            switch (vm.operation) {
+                case 'add':
+                    vm.total = num1 + num2;
+                    break;
+                case 'substract':
+                    vm.total = num1 - num2;
+                    break;
+                case 'multiply':
+                    vm.total = num1 * num2;
+                    break;
+                case 'divide':
+                    vm.total = num1 / num2;
+                    break;
+                
+                default: 
+                    window.alert('unexpected operator');
+                    break;
+            }
+            vm.firstNum = vm.secondNum = null;
+        }
+    }
+
+});
