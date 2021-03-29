6861459353365c4ce79e60b0e4f10deb7eb194ab
Chrissie Nelson <cnelson315@mcad.edu>
2021-03-29 17:48:38 -0500

    create files for week 8.A

---
+++ week8/plants.js
@@ -0,0 +1,33 @@
+let vm = new Vue({
+    el: "#myPlants",
+    data: {
+      plants: [
+        {
+            name: "Snake Plant",
+            botanicalName: "Sansevieria trifasciata",
+            light: "low to bright indirect light",
+            petFriendly: "No",
+        }, {
+            name: "Monstera",
+            botanicalName: "Monstera deliciosa",
+            light: "bright indirect to full sun",
+            petFriendly: "No",
+        }, {
+            name: "Fiddle leaf Fig",
+            botanicalName: "Ficus lyrata",
+            light: "bright indirect light",
+            petFriendly: "No",
+        }, {
+            name: 'Money Tree',
+            botanicalName: "Pachira aquatica",
+            light: "low to bright indirect light",
+            petFriendly: "Yes",
+        }, {
+            name: 'Bamboo Palm',
+            botanicalName: "Chamaedorea seifrizii",
+            light: "low to bright indirect light",
+            petFriendly: "Yes",
+        },
+      ]
+    }
+});
