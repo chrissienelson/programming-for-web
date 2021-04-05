1299177e6a746fb4e44081b7a7cebdc6ede69500
Chrissie Nelson <cnelson315@mcad.edu>
2021-04-05 14:45:02 -0500

    created 9.A assignment files

---
+++ week9/plants.js
@@ -0,0 +1,60 @@
+let vm = new Vue({
+    el: "#myPlants",
+    data: {
+        newPlantObj: {
+            name: '',
+            botanicalName: '',
+            light: '',
+            petFriendly: ''
+        },
+
+      plants: [
+        {
+            name: "Snake Plant",
+            botanicalName: "Sansevieria trifasciata",
+            light: "low to bright indirect light",
+            petFriendly: "No"
+        }, {
+            name: "Monstera",
+            botanicalName: "Monstera deliciosa",
+            light: "bright indirect to full sun",
+            petFriendly: "No"
+        }, {
+            name: "Fiddle leaf Fig",
+            botanicalName: "Ficus lyrata",
+            light: "bright indirect light",
+            petFriendly: "No"
+        }, {
+            name: 'Money Tree',
+            botanicalName: "Pachira aquatica",
+            light: "low to bright indirect light",
+            petFriendly: "Yes"
+        }, {
+            name: 'Bamboo Palm',
+            botanicalName: "Chamaedorea seifrizii",
+            light: "low to bright indirect light",
+            petFriendly: "Yes"
+        }
+      ]
+    },
+    methods: {
+        submitHandler: () => {
+            console.log('submitted');
+            vm.plants = vm.plants.concat(vm.newPlantObj);
+            vm.resetForm();
+        },
+        resetForm: () => {
+            vm.newPlantObj = {
+                name: '',
+                botanicalName: '',
+                light: '',
+                petFriendly: ''
+            };
+        },
+        deleteItem: item => {
+            vm.plants = vm.plants.filter(plant => {
+                return plant !== plant;
+            });
+        }
+    }
+});
