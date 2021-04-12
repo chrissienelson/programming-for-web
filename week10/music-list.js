c952850bbb7d6f4320edb0f7fa086722817aa230
Chrissie Nelson <cnelson315@mcad.edu>
2021-04-12 13:36:32 -0500

    music list exercise

--- week10/music-list.js
+++ week10/music-list.js
@@ -1,11 +1,19 @@
-const vw = new Vue({
+Vue.component('streaming-track', {
+    template: `<div class="track">
+        <h3>{{track.title}}</h3>
+        <div><img v-bind:src="track.cover" alt=""></div>
+        <small>{{track.artist}}</small>
+    </div>`,
+    props: ['track']
+});
+const vm = new Vue({
     el: "#musicApp",
     data: {
         track:[]
     },
     mounted () {
         axios 
-            .get('./week10/music-list.json')
+            .get('music-list.json')
             .then(response => {
                 console.log('response', response);
                 vm.tracks = response.data;
