33c30869daa293fc2bcc5929522e8c28c320c697
Chrissie Nelson <cnelson315@mcad.edu>
2021-04-27 17:08:39 -0500

    12.2

--- week12/sounds.js
+++ week12/sounds.js
@@ -1,11 +1,15 @@
 
 var mySound;
 
+function preload() {
+    mySound = loadSound('zapsplat_nature_forest_birds_wind_trees_leaves_falling_qld_australia_56740.mp3');
+}
+
 function setup() {
-    mySound = loadSound('doorbell.mp3');
+    mySound.setVolume(0.1);
+    mySound.play();
 }
 
 function draw() {
-    mySound.setVolume(0.1);
-    mySound.play();
+
 }
