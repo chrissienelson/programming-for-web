ac4f56145047fa1b49ef8ac72e060da27024a385
Chrissie Nelson <cnelson315@mcad.edu>
2021-05-05 18:28:32 -0500

    final project

--- week14/script.js
+++ week14/script.js
@@ -4,12 +4,16 @@ let startingX = 100;
 let startingY = 100;
 let cards = [];
 const gameState = {
-
+    totalPairs: 9,
+  flippedCards: [],
+  numMatched: 0,
+  attempts: 0,
+  waiting: false
 };
 let cardfaceArray = [];
 let cardback;
 function preload() {
-    cardback = loadImage('images/meow-cardback.png')
+    cardback = loadImage('')
     cardfaceArray = [
         loadImage('images/kitten1.jpg'),
         loadImage('images/kitten2.jpg'),
@@ -17,8 +21,9 @@ function preload() {
         loadImage('images/kitten4.jpg'),
         loadImage('images/kitten5.jpg')
     ]
-
 }
+
+
 function setup() {
     createCanvas(900, 500);
     background('');
@@ -31,6 +36,7 @@ function setup() {
         // remove the used cardface so it doesn't get randomly selected again
         cardfaceArray.splice(randomIdx, 1);
     }
+    selectedFaces = shuffleArray(selectedFaces);
     for (let j = 0; j < 2; j++) {
         for (let i = 0; i < 5; i++) {
             const faceImage = selectedFaces.pop();
@@ -40,15 +46,72 @@ function setup() {
         startingY += 150;
         startingX = 100;
     }
+};
+
+
+//need to finish
+function draw () {
+    background('#ded2bf');
+    if (gameState.numMatched === gameState.totalPairs) {
+        fill('#bf8f36');
+        noStroke();
+        textSize(50);
+        text('You win!', 100, 475);
+        noLoop();
+    }
+    for (let k = 0; k < cards.length; k++) {
+        if(!cards[k].isMatch) {
+            cards[k].face = DOWN;
+        }
+        cards[k].show();
+    }
+    noLoop();
+    gameState.flippedCards.length = 0;
+    gameState.waiting = false;
+    fill (255);
+    textSize(30);
+    text('attempts: ' + gameState.attempts, 100, 425);
+    text('matches: ' + gameState.numMatched, 300, 425);
+    fill('#fffff');
+    textSize(20);
+    text('Match the tone of each music note!', 100, 65);
 }
 
+
 function mousePressed() {
+    if (gameState.waiting) {
+        return;
+    }
     for (let k = 0; k < cards.length; k++) {
-        if (cards[k].didHit(mouseX, mouseY)) {
-            console.log('flipped');
+        // first check flipped acrds length, then trigger flip
+        if (gameState.flippedCards.length < 2 && cards[k].didHit(mouseX, mouseY)) {
+            console.log('flipped', cards[k]);
+            gameState.flippedCards.push(cards[k]);
+        }
+    }
+    if (gameState.flippedCards.length === 2) {
+        gameState.attempts++;
+        if (gameState.flippedCards[0].cardFaceImg === gameState.flippedCards[1].cardFaceImg) {
+            // mark cards as flipped so dont flip back
+                gameState.flippedCards[0].isMatch = true;
+                gameState.flippedCards[1].isMatch = true;
+            // empty the flipped cards array
+                gameState.flippedCards.length = 0;
+            // increment the score
+                gameState.numMatched++;
+                loop();
+        } else {
+            gameState.waiting = true;
+            const loopTimeout = window.setTimeout(() => {
+                loop();
+                window.clearTimeout(loopTimeout);
+            }, 1000)
         }
     }
 }
+//need to finish
+
+
 
 class Card {
     constructor (x, y, cardFaceImg) {
